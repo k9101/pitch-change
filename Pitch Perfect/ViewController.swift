2189d464f1255562fe5ea84f497c55060c693cc8
k9101 <kris9101@gmail.com>
2015-09-07 18:13:56 -0400

    Built UI for the main screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var recordingInProgressLabel: UILabel!
+	@IBOutlet weak var loadingIcon: UIActivityIndicatorView!
+	@IBOutlet weak var stopRecordingButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func recordButtonClicked(sender: UIButton) {
+		//Show the recording in progress label
+		
+		recordingInProgressLabel.hidden = false
+		loadingIcon.startAnimating()
+		stopRecordingButton.hidden = false
+		
+		//TODO: Record the user's voice
+	}
 
+	@IBAction func stopRecording(sender: UIButton) {
+		
+		stopRecordingButton.hidden = true
+		loadingIcon.stopAnimating()
+		recordingInProgressLabel.hidden = true
+		
+	}
+	
 }
 
